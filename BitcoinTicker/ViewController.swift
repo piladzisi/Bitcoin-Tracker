977eea2ae3286eb13315ee9debe051bfbe4e11ec
piladzisi <piladzisi@gmail.com>
2019-05-16 13:35:10 +0300

    fixed symbol

--- BitcoinTicker/ViewController.swift
+++ BitcoinTicker/ViewController.swift
@@ -11,29 +11,21 @@ import Alamofire
 import SwiftyJSON
 
 class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
-   
-    
     let baseURL = "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTC"
     let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
     var finalURL = ""
-
-    //Pre-setup IBOutlets
+    var symbol = "$"
+    
     @IBOutlet weak var bitcoinPriceLabel: UILabel!
     @IBOutlet weak var currencyPicker: UIPickerView!
     
-
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-       currencyPicker.delegate = self
+        currencyPicker.delegate = self
         currencyPicker.dataSource = self
         getBitcoinData(url: baseURL+"USD")
     }
-    
-    
-    
-    //TODO: Place your 3 UIPickerView delegate methods here
-    
+   
     func numberOfComponents(in pickerView: UIPickerView) -> Int { //number of columns in picker
         return 1
     }
@@ -45,60 +37,50 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, attributedTitleForRow row: Int, forComponent component: Int) -> NSAttributedString? {
         let attributedString = NSAttributedString(string: currencyArray[row], attributes: [NSAttributedString.Key.foregroundColor : UIColor.white])
         return attributedString
-        
     }
     
-//    func getSymbol(forCurrencyCode code: String) -> String? {
-//        let locale = NSLocale(localeIdentifier: code)
-//        if locale.displayName(forKey: .currencySymbol, value: code) == code {
-//            let newlocale = NSLocale(localeIdentifier: code.dropLast() + "_en")
-//            return newlocale.displayName(forKey: .currencySymbol, value: code)
-//        }
-//        return locale.displayName(forKey: .currencySymbol, value: code)
-//    }
+    func getSymbol(forCurrencyCode code: String) -> String? {
+        let locale = NSLocale(localeIdentifier: code)
+        if locale.displayName(forKey: .currencySymbol, value: code) == code {
+            let newlocale = NSLocale(localeIdentifier: code.dropLast() + "_en")
+            return newlocale.displayName(forKey: .currencySymbol, value: code)
+        }
+        return locale.displayName(forKey: .currencySymbol, value: code)
+    }
 
+   
+    
     func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
         finalURL = baseURL + currencyArray[row]
+        symbol = getSymbol(forCurrencyCode: currencyArray[row])!
         getBitcoinData(url: finalURL)
    }
    
 //    //MARK: - Networking
-//    /***************************************************************/
 
     func getBitcoinData(url: String) {
        Alamofire.request(url, method: .get)
             .responseJSON { response in
                 if response.result.isSuccess {
-                    print("Sucess! Got the bitcoin data")
                     let bitcoinJSON : JSON = JSON(response.result.value!)
                     self.updateBitcoinData(json: bitcoinJSON)
-
                 } else {
                     print("Error: \(String(describing: response.result.error))")
                     self.bitcoinPriceLabel.text = "Connection Issues"
                 }
             }
-
     }
 
-  
-    
-    
-//
 //    //MARK: - JSON Parsing
-//    /***************************************************************/
-    
+   
     func updateBitcoinData(json : JSON) {
-        
         if let bitcoinResult = json["last"].double {
-        bitcoinPriceLabel.text = "\(bitcoinResult)"
-     }
+        bitcoinPriceLabel.text = "\(self.symbol) \(bitcoinResult)"
+        }
         else {
             bitcoinPriceLabel.text = "Price Unavailable"
         }
-    
     }
-
 
 }
 
